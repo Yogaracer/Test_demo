1bcdb779d5c46972c51d7421a8a8d75296c807b2
Yogaracer <talalasmarius@gmail.com>
2023-04-03 21:34:12 +0300

    Add BasicFirstFormTest lass

---
+++ src/test/java/lt/marius/pom/tests/seleniumeasy/BasicFirstFormTest.java
@@ -0,0 +1,109 @@
+package lt.marius.pom.tests.seleniumeasy;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lt.marius.pom.pages.demoqa.TextBoxPage;
+import lt.marius.pom.pages.seleniumeasy.BasicFirstFormPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class BasicFirstFormTest {
+
+    @BeforeTest
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("start-maximized");
+        options.addArguments("--force-device-scale-factor=1.5");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("http://demo.seleniumeasy.com/basic-first-form-demo.html");
+    }
+
+
+
+    public class TextBoxTest {
+
+        @BeforeMethod
+        public void setUp() {
+            TextBoxPage.open();
+        }
+
+
+    @Test
+    public void testSingleInputField() {
+
+        String inputMessage = "Kelinta siandien diena?";
+        String expectedResult = "Kelinta siandien diena?";
+        String actualResult;
+
+        BasicFirstFormPage.enterMessage(inputMessage);
+        BasicFirstFormPage.clickOnButtonShowMessage();
+        actualResult = BasicFirstFormPage.readMessage();
+
+        Assert.assertEquals(actualResult, expectedResult);
+        String.format("Actual: %s; Expected: %s", actualResult, expectedResult);
+    }
+
+
+    @Test
+    public void testTwoInputFieldsPositive() {
+        String enterValueA = "5";
+        String enterValueB = "8";
+        String expectedResult = "13";
+        String actualResult;
+
+//        WebElement inputEnterValueA = driver.findElement(By.xpath("//input[@id='sum1']"));
+//        inputEnterValueA.sendKeys(enterValueA);
+//
+//        WebElement inputEnterValueB = driver.findElement(By.xpath("//input[@id='sum2']"));
+//        inputEnterValueB.sendKeys(enterValueB);
+//
+//        WebElement buttonSGetTotal = driver.findElement(By.xpath("//form[@id='gettotal']//button"));
+//        buttonSGetTotal.click();
+//
+//        WebElement elementTotal = driver.findElement(By.xpath("//span[@id='displayvalue']"));
+//        actualResult = elementTotal.getText();
+//
+//        Assert.assertEquals(actualResult, expectedResult);
+//        String.format("Actual: %s; Expected: %s", actualResult, expectedResult);
+
+    }
+
+    @Test
+    public void testTwoInputFieldsNegative() {
+        String enterValueA = "5";
+        String enterValueB = "aa";
+        String expectedResult = "NaN";
+        String actualResult;
+
+//        WebElement inputEnterValueA = driver.findElement(By.xpath("//input[@id='sum1']"));
+//        inputEnterValueA.sendKeys(enterValueA);
+//
+//        WebElement inputEnterValueB = driver.findElement(By.xpath("//input[@id='sum2']"));
+//        inputEnterValueB.sendKeys(enterValueB);
+//
+//        WebElement buttonSGetTotal = driver.findElement(By.xpath("//form[@id='gettotal']//button"));
+//        buttonSGetTotal.click();
+//
+//        WebElement elementTotal = driver.findElement(By.xpath("//span[@id='displayvalue']"));
+//        actualResult = elementTotal.getText();
+//
+//        Assert.assertEquals(actualResult, expectedResult);
+//        String.format("Actual: %s; Expected: %s", actualResult, expectedResult);
+
+    }
+}
+
+
