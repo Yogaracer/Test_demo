a1a0d92ea24db8d9ed8507a3114471dceb542526
Yogaracer <talalasmarius@gmail.com>
2023-04-03 21:33:28 +0300

    Add BasicFirstFormPage lass

---
+++ src/test/java/lt/marius/pom/pages/seleniumeasy/BasicFirstFormPage.java
@@ -0,0 +1,29 @@
+package lt.marius.pom.pages.seleniumeasy;
+
+import lt.marius.pom.pages.Common;
+import org.openqa.selenium.By;
+
+public class BasicFirstFormPage {
+    public static void open() {
+        Common.setUpDriver();
+        Common.OpenUrl("http://demo.seleniumeasy.com/basic-first-form-demo.html");
+    }
+
+    public static void closeDriver() {
+        Common.closeDriver();
+    }
+
+    public static void enterMessage(String inputMessage) {
+        Common.sendKeysToElement(
+                By.xpath("//input[@id='user-message']"),
+                inputMessage);
+    }
+
+    public static void clickOnButtonShowMessage() {
+        Common.clickOnElement(By.xpath("//button[contains(@onclick,'show')]"));
+    }
+
+    public static String readMessage() {
+        return Common.getTextFromElement(By.xpath("//*[@id='display']"));
+    }
+}
